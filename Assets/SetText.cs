9684e117616b988fa4bf68db3914277a95d3cacd
karina-ranadive <karinaranadive@gmail.com>
2022-01-29 18:02:07 -0500

    hi

--- Assets/SetText.cs
+++ Assets/SetText.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class SetText : MonoBehaviour
 {
     public Text myText;
+    public Text textHighest;
     // Start is called before the first frame update
     void Start()
     {
         string[] stats = GetComponent<StatisticsWriting>().Stats();
         myText.text = stats[0];
+        textHighest.text = stats[1];
     }
 
 }
